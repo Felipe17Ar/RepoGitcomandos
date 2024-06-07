89837a53aec3120475e9c1bc5011eece3e0ad430
Felipe17 <ariasrodriguezfelipe19@gmail.com>
2024-06-07 10:01:32 +0200

    Anado documentacion al proyecto

--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,6 +1,12 @@
-
+/**
+ * Clase principal
+ */
 public class PrincipalApp {
-
+	
+	/**
+	 * Metodo principal de mi clase
+	 * @param args
+	 */
 	public static void main(String[] args) {
 
 		System.out.println("hola buenas");
